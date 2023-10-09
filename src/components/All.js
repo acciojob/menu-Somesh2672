f63c24e1060cbd0d01f72ee051214458f2329fe6
Somesh Jaiswal <someshjaiswal98@gmail.com>
2023-10-09 22:20:40 +0530

    new

--- src/components/All.js
+++ src/components/All.js
@@ -15,7 +15,7 @@ const All = () => {
         : Items.filter((dish) => dish.category === currentCategory);
 
     return dishList.map((dish) => (
-      <div key={dish.id}>
+      <div  key={dish.id}>
         <img src={dish.img} alt={dish.title} />
         <h1>{dish.title}</h1>
         <p>${dish.price}</p>
@@ -28,10 +28,10 @@ const All = () => {
     <div>
       <div className="Navbar">
         <h1>Our Menu</h1>
-        <button onClick={() => handleButtonClick("All")}>All</button>
-        <button onClick={() => handleButtonClick("breakfast")}>Breakfast</button>
-        <button onClick={() => handleButtonClick("lunch")}>Lunch</button>
-        <button onClick={() => handleButtonClick("shakes")}>Shakes</button>
+        <button id="main" onClick={() => handleButtonClick("All")}>All</button>
+        <button id="filter-btn-1" onClick={() => handleButtonClick("breakfast")}>Breakfast</button>
+        <button id="filter-btn-2" onClick={() => handleButtonClick("lunch")}>Lunch</button>
+        <button id="filter-btn-3" onClick={() => handleButtonClick("shakes")}>Shakes</button>
       </div>
 
       <div className="Dishes">{renderDishes()}</div>
